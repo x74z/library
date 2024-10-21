48d8b443a8c91ea030ca38b2741e006898bea314
x74z <166341818+x74z@users.noreply.github.com>
2024-10-21 21:16:15 +0100

    made initial logic to add books to page

--- code.js
+++ code.js
@@ -1,4 +1,20 @@
-const myLibrary = [];
+// All of the books will be stored here.
+const myLibrary = [
+  {
+    title: "1984",
+    author: "George Orwell",
+    pages: 328,
+    haveRead: true,
+  },
+
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    pages: 277,
+    haveRead: false,
+  },
+];
+
 function Book(title, author, pages, haveRead) {
   (this.title = title),
     (this.author = author),
@@ -6,11 +22,34 @@ function Book(title, author, pages, haveRead) {
     (this.haveRead = haveRead);
 }
 
-function addBookToLibrary(book) {
-  for (const key in book) {
+const booksContainer = document.querySelector(".books-container");
+function addBookToLibrary() {
+  for (const book of myLibrary) {
+    const bookTitle = document.createElement("p");
+    bookTitle.className = "book-title";
+    const bookAuthor = document.createElement("p");
+    bookAuthor.className = "book-author";
+    const bookPages = document.createElement("p");
+    bookPages.className = "book-pages";
+    const bookRead = document.createElement("p");
+    bookRead.className = "book-read";
+
     //Itirate trough every property to add it to a card
     //inside the library .book-container
+
+    const bookCard = document.createElement("div");
+    bookCard.className = "book-card";
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookPages.textContent = book.pages;
+    bookRead.textContent = book.haveRead;
+
+    //keep at the end after changing the content
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookRead);
+    booksContainer.appendChild(bookCard);
   }
 }
-
-
