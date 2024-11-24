37abd172aa66bc20810b776b7d2c003758b600ac
x74z <166341818+x74z@users.noreply.github.com>
2024-11-24 12:37:53 +0000

    Refactor code

---
+++ add-to-dom.js
@@ -0,0 +1,61 @@
+import { Book } from "./code";
+
+function addBookCardChildren(bookCard) {
+  // Make all the nodes that will be appended and add their classes
+  const bookTitle = document.createElement("p");
+  bookTitle.className = "book-title";
+  const bookAuthor = document.createElement("p");
+  bookAuthor.className = "book-author";
+  const bookPages = document.createElement("p");
+  bookPages.className = "book-pages";
+  const bookRead = document.createElement("p");
+  bookRead.className = "book-read";
+  const bookRemoveButton = document.createElement("button");
+  bookRemoveButton.className = "book-x-button";
+  const bookChangeReadStatus = document.createElement("button");
+  bookChangeReadStatus.className = "book-status";
+  // Append everything.
+  bookCard.appendChild(bookRemoveButton);
+  bookCard.appendChild(bookTitle);
+  bookCard.appendChild(bookAuthor);
+  bookCard.appendChild(bookPages);
+  bookCard.appendChild(bookRead);
+  bookCard.appendChild(bookChangeReadStatus);
+}
+export function addBookCardToDOM(book) {
+  // Create the card the book will be in.
+  const bookCard = document.createElement("div");
+  bookCard.className = "book-card";
+  bookTitle.textContent = book.title;
+  bookAuthor.textContent = book.author;
+  bookPages.textContent = book.pages;
+  bookRead.textContent = book.haveRead;
+
+  addBookCardChildren(bookCard);
+
+  //Add the final div to its container
+  const booksContainer = document.querySelector(".books-container");
+  booksContainer.appendChild(bookCard);
+  //Add a reference to the html bookCard in the object
+  book.htmlReference = bookCard;
+
+  // add the listener for the remove button
+  bookRemoveButton.addEventListener("click", () => {
+    book.removeBook();
+  });
+  //Handle the status button
+  bookChangeReadStatus.addEventListener("click", () => {
+    // TODO : add classes for styling of status
+    let newStatus = book.changeReadStatus();
+    bookRead.textContent = newStatus;
+  });
+
+  return;
+}
+
+export function addSavedBooksToDOM() {
+  Book.getBooks().forEach((book) => {
+    addBookCardToDOM(book);
+  });
+}
+addSavedBooksToDOM();
--- code.js
+++ code.js
@@ -1,14 +1,18 @@
 // All of the books will be stored here.
-const myLibrary = [];
 
-class Book {
+import { addBookCardToDOM } from "./add-to-dom";
+
+export class Book {
+  static books = [];
+  static getBooks = () => books;
+
   constructor(title, author, pages, haveRead) {
-    (this.title = title),
-      (this.author = author),
-      (this.pages = pages),
-      (this.haveRead = haveRead),
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
     // The html reference is to be able to access the dataset
-      (this.htmlReference = "");
+    Book.books.push(this);
   }
   changeReadStatus() {
     if (this.haveRead === "yes") {
@@ -19,100 +23,39 @@ class Book {
       return "yes";
     }
   }
+  removeBook() {
+    Book.getBooks().splice(Book.books.indexOf(this), 1);
+  }
 }
 
-
-const booksContainer = document.querySelector(".books-container");
 // function addDataAtributte(book) {}
 
-function updateBooksIndexes() {
-  myLibrary.forEach((e) => {
-    e.htmlReference.dataset.index = myLibrary.indexOf(e);
-  });
-}
+// function updateBooksIndexes() {
+//   Book.books.forEach((e) => {
+//     e.htmlReference.dataset.index = Book.books.indexOf(e);
+//   });
+// }
 
-function addBookCard(book) {
-  // Make all the nodes that will be appended and add their classes
-  const bookTitle = document.createElement("p");
-  bookTitle.className = "book-title";
-  const bookAuthor = document.createElement("p");
-  bookAuthor.className = "book-author";
-  const bookPages = document.createElement("p");
-  bookPages.className = "book-pages";
-  const bookRead = document.createElement("p");
-  bookRead.className = "book-read";
-  const bookRemoveButton = document.createElement("button");
-  bookRemoveButton.className = "book-x-button";
-  const bookChangeReadStatus = document.createElement("button");
-  bookChangeReadStatus.className = "book-status";
-
-  // Create the card the book will be in.
-  const bookCard = document.createElement("div");
-  bookCard.className = "book-card";
-  bookTitle.textContent = book.title;
-  bookAuthor.textContent = book.author;
-  bookPages.textContent = book.pages;
-  bookRead.textContent = book.haveRead;
-
-  // Add the corresponding data attribute, acording to index
-  const bookIndex = myLibrary.indexOf(book);
-  bookCard.setAttribute("data-index", bookIndex);
-
-  // Append everything.
-  bookCard.appendChild(bookRemoveButton);
-  bookCard.appendChild(bookTitle);
-  bookCard.appendChild(bookAuthor);
-  bookCard.appendChild(bookPages);
-  bookCard.appendChild(bookRead);
-  bookCard.appendChild(bookChangeReadStatus);
-  //Add the final div
-  booksContainer.appendChild(bookCard);
-  //Add a reference to the html bookCard in the object
-  book.htmlReference = bookCard;
-  // add the listener for the remove button
-  bookRemoveButton.addEventListener("click", () => {
-    const currentBookIndex = bookCard.dataset.index;
-    myLibrary.splice(currentBookIndex, 1);
-    booksContainer.removeChild(bookCard);
-    updateBooksIndexes();
-  });
-  //Handle the status button
-  bookChangeReadStatus.addEventListener("click", () => {
-    // TODO : add classes for styling of status
-    let newStatus = book.changeReadStatus();
-    bookRead.innerText = newStatus;
-  });
-
-  return;
-}
-function addBookToLibrary(bookObject) {
-  // If it is an array, add all the objects from the array
-  if (!Array.isArray(bookObject)) {
-    for (const book of myLibrary) {
-      addBookCard(book);
-    }
-    return;
-  }
-
-  //if only one book
-  addBookCard(bookObject);
-  return;
-}
-addBookToLibrary();
 
 // Add book functionality
-const addBookDialog = document.querySelector(".add-book-dialog");
-//close by default
-const addBookButton = document.querySelector("#add-book");
-addBookButton.addEventListener("click", () => {
-  //code, watch the tutorial about dialogs and modals, to show it when clicked
-  addBookDialog.show();
-});
-//Add book close button
-const closeBookButton = document.querySelector("#close-dialog-button");
-closeBookButton.addEventListener("click", () => {
-  addBookDialog.close();
-});
+const addBookDialog = (() => {
+  // Add the listeners on load
+  const dialog = document.querySelector(".add-book-dialog");
+  document.querySelector("#add-book").addEventListener("click", () => {
+    //code, watch the tutorial about dialogs and modals, to show it when clicked
+    dialog.show();
+  });
+  //Add book close button
+  document .querySelector("#close-dialog-button") .addEventListener("click", () => {
+      dialog.close();
+    });
+
+  // Show the functionality to close the dialogs
+  const close = () => dialog.close();
+  const show = () => dialog.show();
+
+  return { close, show };
+})();
 
 const addBookDialogForm = document.querySelector("body > dialog > div > form");
 addBookDialogForm.addEventListener("submit", (e) => {
@@ -124,8 +67,7 @@ addBookDialogForm.addEventListener("submit", (e) => {
   let read = addBookDialogForm.read.value;
   // Add the book to the array and the card
   const bookObject = new Book(title, author, pages, read);
-  myLibrary.push(bookObject);
-  addBookCard(bookObject);
+  addBookCardToDOM(bookObject);
   //Reset all the values
   addBookDialogForm.reset();
 
